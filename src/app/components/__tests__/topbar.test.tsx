ae2db31d6e75c7811684c42ab0af661fe9593e74
Hasmukh Mistry <hasmukhmistry137@gmail.com>
2021-11-28 23:35:58 +0530

    Fixes test for topbar component

--- src/app/components/__tests__/topbar.test.tsx
+++ src/app/components/__tests__/topbar.test.tsx
@@ -6,7 +6,7 @@ const shallowRenderer = createRenderer();
 
 describe('<TopBar />', () => {
   it('should match snapshot', () => {
-    shallowRenderer.render(<TopBar timerSeconds={60} userScore={0}/>);
+    shallowRenderer.render(<TopBar seconds={60} currentUser={{ id: -1, points: 0, ip_addr: '', timestamp: '' }} setCurrentUser={{}} currentScore={0} setCurrentScore={{}} votingEnabled={false} setVotingEnabled={0}/>);
     const renderedOutput = shallowRenderer.getRenderOutput();
     expect(renderedOutput).toMatchSnapshot();
   });
